ca66e947099f624ccb01f6d6cb6b570eb4d21c9d
Nick Sabine <nsabine@gmail.com>
2015-10-01 17:51:39 -0400

    fixed syntax errors in run.sh

--- run.sh
+++ run.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 if [ -n "${CASSANDRA_PORT_9160_TCP_ADDR}" ]; then
-  cat >> ${TITAN_PROPERTIES} <<END
+  cat >> ${TITAN_PROPERTIES} <<EOL
 storage.backend=cassandra
 storage.hostname=${CASSANDRA_PORT_9160_TCP_ADDR}
-END
+EOL
 
 if [ -n "${ELASTICSEARCH_PORT_9300_TCP_ADDR}" ]; then
-  cat >> ${TITAN_PROPERTIES} <<END
+  cat >> ${TITAN_PROPERTIES} <<EOL
 index.search.backend=elasticsearch
 index.search.elasticsearch.client-only=true
 index.search.hostname=${ELASTICSEARCH_PORT_9300_TCP_ADDR}
-END
+EOL
 
-cat > ${TITAN_HOME}/init.groovy <<END
+cat > ${TITAN_HOME}/init.groovy <<EOL
 g = TitanFactory.open("${TITAN_PROPERITES}")
-println "The graph \'g\' was opened using ${TITAN_PROPERTIES}"
-END
+println "The graph g was opened using ${TITAN_PROPERTIES}"
+EOL
 
 exec ${TITAN_HOME}/bin/gremlin.sh ${TITAN_HOME}/init.groovy
 
